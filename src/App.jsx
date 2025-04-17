b22d4396d5a9421ed89ae9b9d4bd8d1a00148b95
MuhRezkyPasuloi <resky.pasuloi@gmail.com>
2025-04-17 17:20:21 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-// import Dashboard from "../src/page/Dashboard";
-// import UserDash from "./page/userDash";
-// import Trip from "./page/Trip";
-// import AddOpenTrip from "./page/AddOpenTrip";
-// import AddPrivateTrip from "./page/AddPrivateTrip";
-// import TranOpenTrip from "./page/TransOpenTrip";
-// import DetailOpenTransaksi from "./page/DetOpenTran";
-// import DetPrivTran from "./page/DetPrivTrip";
-// import Laporan from "./page/LaporanTrip";
-// import GuidePorter from "./page/GuidePorterPage";
-// import Sidebar from "./components/Sidebar";
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes"
+import React from 'react'
 
 const App = () => {
-  return <AppRoutes />;
-};
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,52 +1,120 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
-import Dashboard from "../page/Dashboard";
-import UserDash from "../page/userDash";
-import OpenTrip from "../page/OpenTrip";
-import PrivateTrip from "../page/PrivateTrip";
-import TranOpenTrip from "../page/TransOpenTrip";
-import TranPrivTrip from "../page/TransPrivTrip";
-import Laporan from "../page/LaporanTrip";
-import GuidePorter from "../page/GuidePorterPage";
-import Login from "../page/Login";
-import DetailOpenTransaksi from "../page/DetOpenTran";
-import DetailPrivateTransaksi from "../page/DetPrivTrip";
-import AddOpenTrip from "../page/AddOpenTrip";
-import EditOpenTrip from "../page/EditOpenTrip";
-import AddPrivTrip from "../page/AddPrivateTrip";
-import UserDetailView from "../page/UserDetailView";
-import ViewOpenTrip from "../page/ViewOpenTrip";
-import ViewPrivateTrip from "../page/ViewPrivateTrip";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react'
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+import LandingPage from "../pages/LandingPage";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+import AboutUs from "../pages/AboutUs";
+import Trip from "../pages/Trip";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Profile from "../pages/Profile";
+import TripDetail from "../pages/TripDetail";
+import Book from "../pages/Book";
+import BookingDetail from "../pages/BookingDetail";
+import UpdateProfile from "../pages/UpdateProfile";
+import UpdatePassword from "../pages/UpdatePassword";
+import DashRoutes from "./DashRoutes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AppRoutes = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (token) {
+            try {
+                const decoded = jwtDecode(token);
+                const role = decoded.role;
+
+                if (role === "admin") {
+                    navigate("/dashboard");
+                } else {
+                    navigate("/");
+                }
+            } catch (error) {
+                console.error("Invalid token", error);
+                navigate("/login"); // atau redirect ke login kalau token rusak
+            }
+        }
+    }, [Cookies.get("token")])
+
     return (
-        <Router>
-            <div className="flex h-screen"> 
-                <Sidebar />
-                <div className="flex-1 p-4 overflow-y-auto"> {/* Main content area */}
-                    <Routes>
-                        <Route path="/" element={<Login />} /> {/* Halaman login */}
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/user" element={<UserDash />} />
-                        <Route path="/opentrip" element={<OpenTrip />} />
-                        <Route path="/privtrip" element={<PrivateTrip />} />
-                        <Route path="/booking-open" element={<TranOpenTrip />} />
-                        <Route path="/booking-private" element={<TranPrivTrip />} />
-                        <Route path="/laporan" element={<Laporan />} />
-                        <Route path="/guideporter" element={<GuidePorter />} />
-                        <Route path="/booking/open/:id" element={<DetailOpenTransaksi />} />
-                        <Route path="/booking/private/:id" element={<DetailPrivateTransaksi />} />
-                        <Route path="/add-open-trip" element={<AddOpenTrip />} />
-                        <Route path="/edit-open-trip/:id" element={<EditOpenTrip />} />
-                        <Route path="/add-private-trip" element={<AddPrivTrip />} />
-                        <Route path="/view-private-trip/:id" element={<ViewPrivateTrip />} />
-                        <Route path="/view-open-trip/:id" element={<ViewOpenTrip />} />
-                        <Route path="/user/:id" element={<UserDetailView />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
+        <>
+            <ToastContainer />
+
+            <Routes>
+                <Route path="/" element={
+                    <>
+                        <Navbar />
+                        <LandingPage />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/tentangkami" element={
+                    <>
+                        <Navbar />
+                        <AboutUs />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/daftartrip" element={
+                    <>
+                        <Navbar />
+                        <Trip />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/trip/:mountain_name" element={
+                    <>
+                        <Navbar />
+                        <TripDetail />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard/*" element={<DashRoutes />} />
+                <Route path="/profile" element={
+                    <>
+                        <Navbar />
+                        <Profile />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/bookings" element={
+                    <>
+                        <Navbar />
+                        <Book />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/bookings/:mountain_name" element={
+                    <>
+                        <Navbar />
+                        <BookingDetail />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/updateprofile" element={
+                    <>
+                        <Navbar />
+                        <UpdateProfile />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/updatepassword" element={
+                    <>
+                        <Navbar />
+                        <UpdatePassword />
+                        <Footer />
+                    </>
+                } />
+            </Routes>
+        </>
+    )
 }
 
 export default AppRoutes;
